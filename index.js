f045c53e8e96225afb91576a45e54ffcd5795a3f
Josh S <josh@operationspark.org>
2023-08-12 16:44:41 -0500

    added cors

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
